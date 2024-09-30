55815e97cd7c4e3cd5d855d6a6748cc9731dd629
Alex Parker <128879861+Soumyaranjan-17@users.noreply.github.com>
2024-09-30 15:16:39 +0530

    Update OOP_Java
    single inheritance 30/09/2024

---
+++ OOP_Java/SingleInheritance.java
@@ -0,0 +1,62 @@
+import java.util.Scanner;
+
+class Father {
+    String Fname;
+    int age;
+
+    // Method to get father's details
+    void getData(String fname, int age) {
+        this.Fname = fname;
+        this.age = age;
+    }
+
+    // Method to display father's details
+    void display() {
+        System.out.println("Father's name = " + Fname + ", Father's age = " + age);
+    }
+}
+
+class Son extends Father {
+    String Sname;
+    int Sage;
+
+    // Method to get son's details along with father's details
+    void get(String fname, int fage, String sname, int sage) {
+        // Using the parent class's getData method for father
+        getData(fname, fage);
+        this.Sname = sname;
+        this.Sage = sage;
+    }
+
+    // Method to display son's details along with father's details
+    void disp() {
+        display();  // Calling display method from Father class
+        System.out.println("Son's name = " + Sname + ", Son's age = " + Sage);
+    }
+}
+
+public class SingleInheritanceDemo {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input for Father
+        System.out.print("Enter name of father: ");
+        String fname = sc.nextLine();
+        System.out.print("Enter age of father: ");
+        int fage = sc.nextInt();
+        sc.nextLine();  // consume newline
+
+        // Input for Son
+        System.out.print("Enter name of son: ");
+        String sname = sc.nextLine();
+        System.out.print("Enter age of son: ");
+        int sage = sc.nextInt();
+
+        // Create a Son object
+        Son son = new Son();
+        
+        // Set and display data
+        son.get(fname, fage, sname, sage);
+        son.disp();
+    }
+}
