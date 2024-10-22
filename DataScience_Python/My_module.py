053c4191d140535394c6fcc26d99c716d8f6ecef
Soumya <www.soumyaranjansahoo17@gmail.com>
2024-10-22 11:36:25 +0530

    Module program completed

--- DataScience_Python/My_module.py
+++ DataScience_Python/My_module.py
@@ -3,20 +3,14 @@ PI = 3.141
 def area(a):
     return a*a
 
-def area(l,b):
+def area2(l,b):
     return l*b
 
 def perimeter(a):
     return 4*a
 
-def perimeter(l,b):
+def perimeter2(l,b):
     return 2*(l+b)
-
-def vol(a):
-    return a*a*a
-
-def vol(l, b, h):
-    return l*b*h
 
 def factorial(n):
     if n == 1:
@@ -28,11 +22,16 @@ class Car:
     color = "Transparent"
     milage = 0
     top_speed = 0
-    def __init__(self, color, milage, top_speed) -> None:
+
+    def __init__(self, color: str, milage: float, top_speed: float) -> None:
         self.color = color
         self.milage = milage
         self.top_speed = top_speed
 
-    def start(self):
-        print(f"This {self.color} car is starting")
-        
+    def start(self) -> str:
+        return f"The {self.color} car with a top speed of {self.top_speed} km/h is starting."
+
+    def display_info(self) -> str:
+        return (f"Car Color: {self.color}\n"
+                f"Milage: {self.milage} km\n"
+                f"Top Speed: {self.top_speed} km/h")
